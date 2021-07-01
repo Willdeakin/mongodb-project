3e738670dfc89906ef0cec04b9801587f0e627c0
Willdeakin <willdeakin@outlook.com>
2021-07-01 13:12:45 +0000

    completed mongo lessons

--- mongo.py
+++ mongo.py
@@ -9,22 +9,24 @@ DATABASE = "myFirstDB"
 COLLECTION = "celebrities"
 
 
-
 def mongo_connect(url):
     try:
         conn = pymongo.MongoClient(url)
         print("Mongo is connected")
         return conn
     except pymongo.errors.ConnectionFailure as e:
-        print("Could not connect to MongoDb: %s") % e
+        print("Could not connect to MongoDB: %s") % e
 
 
 conn = mongo_connect(MONGO_URI)
 
 coll = conn[DATABASE][COLLECTION]
 
-documents = coll.find()
+coll.update_one({"nationality":"american"}, {"$set": {"hair_colour":"maroon"}})
+
+
+documents = coll.find({"nationality": "american"})
+
 
 for doc in documents:
     print(doc)
-
